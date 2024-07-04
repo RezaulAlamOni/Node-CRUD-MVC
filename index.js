a54907bde9389a506568c8e4a04499a16c8728aa
RezaulAlamOni <rzalam93@gmail.com>
2024-07-04 14:52:06 +0600

    delete product

--- index.js
+++ index.js
@@ -62,6 +62,20 @@ mongoose.connect(url)
             }
         });
 
+        // delete product
+        app.delete('/api/product/:id', async (req, res) => {
+            const { id } = req.params;
+            try {
+                const product = await Product.findByIdAndDelete(id);
+                if (!product) {
+                    return res.status(404).json({ message: 'Product not found' });
+                }
+                res.status(200).json({message: 'Product deleted successfully' });
+            } catch (error) {
+                res.status(500).json({ error: error.message });
+            }
+        });
+
     })
     .catch(() => {
         console.log("Connection failed!");
